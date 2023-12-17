4bcadadcf9e4a543bc3d73733790aa938a40af10
Yttrium_32 <shresthprasad7@gmail.com>
2023-12-17 13:54:24 +0530

    Added a get route

--- backend/app/api/routes/skills.py
+++ backend/app/api/routes/skills.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Body, Depends
-from starlette.status import HTTP_201_CREATED
+from fastapi import APIRouter, Body, Depends, HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 from app.models.skills import CreateSkill, SkillPublic
 from app.db.repositories.skills import SkillsRepository
@@ -18,6 +18,17 @@ async def get_all_skills() -> List[dict]:
     ]
 
     return skills
+
+@router.get("/{id}/", response_model=SkillPublic, name="skills:get-skill-by-id")
+async def get_skill_by_id(
+    id: int,
+    skills_repo: SkillsRepository = Depends(get_repository(SkillsRepository))
+    ) -> SkillPublic:
+    skill = await skills_repo.get_skill_by_id(id=id)
+    if not skill:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No skill found with that id.")
+
+    return skill
 
 @router.post("/", response_model=SkillPublic, name="skills:create-skill", status_code=HTTP_201_CREATED)
 async def create_new_skill(
--- backend/app/db/repositories/skills.py
+++ backend/app/db/repositories/skills.py
@@ -7,6 +7,12 @@ CREATE_SKILLS_QUERY = """
     RETURNING id, name, contact_info, test_data
 """
 
+GET_SKILL_BY_ID_QUERY = """
+    SELECT id, name, contact_info, test_data
+    FROM skills
+    WHERE id = :id
+"""
+
 class SkillsRepository(BaseRepository):
     """
     All database actions associated with the Cleanings resource
@@ -15,6 +21,14 @@ class SkillsRepository(BaseRepository):
     async def create_skills(self, *, new_skill: CreateSkill) -> SkillInDB:
         query_values = new_skill.dict()
         skill = await self.db.fetch_one(query=CREATE_SKILLS_QUERY, values=query_values)
+
+        return SkillInDB(**skill)
+
+    async def get_skill_by_id(self, *, id: int):
+        skill = await self.db.fetch_one(query=GET_SKILL_BY_ID_QUERY, values={"id": id})
+
+        if not skill:
+            return None
 
         return SkillInDB(**skill)
 
