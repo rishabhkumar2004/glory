3fec56f7b5dd76e2b983707bd084230f15838bc4
Yttrium_32 <shresthprasad7@gmail.com>
2024-01-20 19:23:35 +0530

    add query to create a new user

---
+++ backend/app/routers/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from connection import engine
+from ..schema import skills, users
+from ..dependencies import get_db
+from .. import models, crud
+
+models.Base.metadata.create_all(bind=engine)
+router = APIRouter()
+
+@router.post("/users/", response_model=users.UserPublic, tags=["users"])
+def create_user(user: users.UserCreate, db: Session = Depends(get_db)):
+    email_in_use = crud.get_user_by_email(db=db, email=user.email)
+    username_in_use = crud.get_user_by_username(db=db, username=user.username)
+    if email_in_use:
+        raise HTTPException(status_code=400, detail="Email is already in use")
+    if username_in_use:
+        raise HTTPException(status_code=400, detail="Username is already in use")
+    return crud.create_user(db=db, user=user)
+
