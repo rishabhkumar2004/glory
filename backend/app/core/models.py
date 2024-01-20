b3e347d3523ba90f6e18102c5aaf9d2e3791f2d9
Yttrium_32 <shresthprasad7@gmail.com>
2024-01-20 18:38:57 +0530

    add username field to users table

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -6,7 +6,8 @@ from core.connection import Base
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True)
     password = Column(String)
 
     skills = relationship("Skills", back_populates="user")
